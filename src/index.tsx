1869c79d7098ace988a00782545dd5324a6a34f3
Kristian Quirapas <kmquirapas@gmail.com>
2022-07-15 02:22:57 +0800

    Add GlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,17 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
+import { GlobalStyle } from "./constants";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <BrowserRouter>
       <Routes>
-        <Route element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
