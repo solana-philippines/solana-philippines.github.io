b30e091b607aa27480b1cece56eeb9ae4e4ecf25
Kristian Quirapas <kmquirapas@gmail.com>
2022-07-15 02:22:57 +0800

    Add GlobalStyle

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,10 @@
-export {};
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export { GlobalStyle };
