8bdc1e5818de2e2903214539fef6191db07b2853
Kristian Quirapas <kmquirapas@gmail.com>
2022-07-16 04:03:50 +0800

    Add new component

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+import Body from "../../components/Body";
+import Navbar from "../../components/Navbar";
+import Header from "../../components/Header";
+
+export default function NotFound() {
+  return (
+    <>
+      <Navbar />
+      <Body>
+        <StyledMain>
+          <StyledHeader className="subheader-text-size">
+            Page Not Found
+          </StyledHeader>
+          <StyledSubheader className="section-text-size">
+            Uh-oh! Page not found
+          </StyledSubheader>
+        </StyledMain>
+      </Body>
+    </>
+  );
+}
+
+const StyledMain = styled.main`
+  min-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledHeader = styled.header`
+  font-family: Lato Black;
+  background: #fcd116;
+  background: linear-gradient(to right, #fcd116 0%, #0038a8 51%, #ce1126 66%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  @media only screen and (max-width: 768px) {
+    margin-top: 0.5em;
+    font-size: 4em;
+  }
+`;
+
+const StyledSubheader = styled.header`
+  font-family: Lato Regular;
+`;
+
+const StyledGithub = styled.img`
+  width: 7em;
+  filter: invert(87%) sepia(100%) saturate(29%) hue-rotate(129deg) brightness(108%) contrast(106%);
+    }
+
+  @media only screen and (max-width: 768px) {
+    width: 5em;
+  }
+`;
